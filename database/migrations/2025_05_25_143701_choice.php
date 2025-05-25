ab1c59d45dcb83045160899baad3fa3082f68241
Droplet <darellpetualang@gmail.com>
2025-05-25 21:44:38 +0700

    database scheme (current 25-05-2025)

---
+++ database/migrations/2025_05_25_143701_choice.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('node_id')->constrained()->onDelete('cascade');
+            $table->string('label');
+            $table->foreignId('next_node_id')->constrained('nodes')->onDelete('cascade');
+            $table->boolean('key_choice')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('choices');
+    }
+};
